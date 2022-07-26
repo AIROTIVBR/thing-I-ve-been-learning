4f7bb8f2efeff58ad3e522a123c6649dc7337563
Vitória Amorim <101279006+AIROTIVBR@users.noreply.github.com>
2022-07-26 02:39:37 -0300

    Add files via upload

--- test2.cpp
+++ test2.cpp
@@ -1,52 +1,33 @@
-#include <stdio.h> 
+#include<stdio.h>
+#include<iostream>
 
-int main() {
-	int mes;
-	printf("digite o numero do seu mes de nascimento: ");
-	scanf("%d", &mes);
+
+
+int main(void)
+{
+	char name[100];
+	char surname[100];
+    char fullname[100];
+    bool havesname;
 	
-	if(mes> 12) {
-		printf("acho que a matematica ta meia errada bro, um ano tem 12 meses visse.");
-	}
-	else{
+	printf("Please enter your name:");
+	scanf("%s",&name); 
+	printf("Your name is %s \n", name);
+	printf("Beatiful name :)\n");
+	printf("Do you have a surname ?\n");
+	printf("If yes, so anwser 1, if you dont, anwser 0\n");
+	scanf("%d", &havesname);
+	
+	if(havesname)
+	{
+	    printf("So whats your surname ?");
+	    scanf("%s", &surname);
+	    printf("So your fullname is : %s %s, thats really unique, nice to meet you ;)\n",&name,surname);
+ 	    printf("Thanks for anwsering !!");
     }
-    if(mes==1) {
-		printf("o deus grego que corresponde a seu mes de nascimento eh Atena.");
-	} 
-	else if(mes==2) {
-		printf("o deus grego que corresponde a seu mes de nascimento eh Poseidon.");
+    else{
+    	printf("Thats unusual...keep your secrets then -_-");
 	}
-	else if(mes == 3) {
-		printf("o deus grego que corresponde a seu mes de nascimento eh Ares.");
-	} 
-	else if(mes == 4) {
-		printf("o deus grego que corresponde a seu mes de nascimento eh Demeter.");
-    } 
-	else if(mes == 5) {
-		printf("o deus grego que corresponde a seu mes de nascimento eh Hermes.");
-    } 
-	else if(mes == 6) {
-		printf("o deus grego que corresponde a seu mes de nascimento eh Hera.");
-	}
-	else if(mes== 7) {
-		printf("o deus grego que corresponde a seu mes de nascimento eh Apolo.");
-	}
-	else if(mes== 8) {
-		printf("o deus grego que corresponde a seu mes de nascimento eh Artemis.");
-	}
-	else if(mes== 9) {
-		printf("o deus grego que corresponde a seu mes de nascimento eh Afrodite.");
-	}
-	else if(mes== 10) {
-		printf("o deus grego que corresponde a seu mes de nascimento eh Hades.");
-	}
-	else if(mes== 11) {
-		printf("o deus grego que corresponde a seu mes de nascimento eh Zeus.");
-	}
-	else if(mes== 12) {
-		printf("o deus grego que corresponde a seu mes de nascimento eh Hefesto.");
-	}
-   else {
-   }
-    
+	
 }
+
