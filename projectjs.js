628070bb1893612beaf98a90dbafd67e3c4cb99d
Vitória Amorim <101279006+AIROTIVBR@users.noreply.github.com>
2022-09-20 00:43:11 -0300

    Delete projectjs.js

--- projectjs.js
+++
@@ -1,6 +0,0 @@
-function metersconverter(value) {
-    document.getElementById("centimeters").innerHTML=value*100;
-    document.getElementById("kilometers").innerHTML=value/1000;
-    document.getElementById("foot").innerHTML=value*3.28;
-    document.getElementById("miles").innerHTML=value/6.210;
-}
